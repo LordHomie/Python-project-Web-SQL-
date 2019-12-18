f30295621ebb7e4d28bfcde496015b081a5aa035
LordHomie <56148118+LordHomie@users.noreply.github.com>
2019-12-18 13:29:04 +0300

    Update formflask.py

--- Flaskproject/formflask.py
+++ Flaskproject/formflask.py
@@ -224,117 +224,51 @@ def capitals(city, limit):
     for div in status:
         STATUS.append(div.img.get('title'))
 
-    day1 = DATA[0][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[0][10:] + ",&nbsp;&nbsp;" + STATUS[0] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day2 = DATA[1][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[1][10:] + ",&nbsp;&nbsp;" + STATUS[1] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day3 = DATA[2][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[2][10:] + ",&nbsp;&nbsp;" + STATUS[2] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day4 = DATA[3][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[3][10:] + ",&nbsp;&nbsp;" + STATUS[3] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day5 = DATA[4][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[4][10:] + ",&nbsp;&nbsp;" + STATUS[4] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day6 = DATA[5][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[5][10:] + ",&nbsp;&nbsp;" + STATUS[5] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day7 = DATA[6][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[6][10:] + ",&nbsp;&nbsp;" + STATUS[6] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day8 = DATA[7][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[7][10:] + ",&nbsp;&nbsp;" + STATUS[7] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day9 = DATA[8][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[8][10:] + ",&nbsp;&nbsp;" + STATUS[8] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day10 = DATA[9][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[9][10:] + ",&nbsp;&nbsp;" + STATUS[9] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day11 = DATA[10][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[10][10:] + ",&nbsp;&nbsp;" + STATUS[10] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day12 = DATA[11][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[11][10:] + ",&nbsp;&nbsp;" + STATUS[11] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day13 = DATA[12][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[12][10:] + ",&nbsp;&nbsp;" + STATUS[12] + "&nbsp;&nbsp;" + "<br/><br/>"
-    day14 = DATA[13][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[13][10:] + ",&nbsp;&nbsp;" + STATUS[13] + "&nbsp;&nbsp;" + "<br/><br/>"
 
-    Day1 = DATA[0][:9] + ", " + DATA[0][10:] + ", " + STATUS[0] + "|"
-    Day2 = DATA[1][:9] + ", " + DATA[1][10:] + ", " + STATUS[1] + "|"
-    Day3 = DATA[2][:9] + ", " + DATA[2][10:] + ", " + STATUS[2] + "|"
-    Day4 = DATA[3][:9] + ", " + DATA[3][10:] + ", " + STATUS[3] + "|"
-    Day5 = DATA[4][:9] + ", " + DATA[4][10:] + ", " + STATUS[4] + "|"
-    Day6 = DATA[5][:9] + ", " + DATA[5][10:] + ", " + STATUS[5] + "|"
-    Day7 = DATA[6][:9] + ", " + DATA[6][10:] + ", " + STATUS[6] + "|"
-    Day8 = DATA[7][:9] + ", " + DATA[7][10:] + ", " + STATUS[7] + "|"
-    Day9 = DATA[8][:9] + ", " + DATA[8][10:] + ", " + STATUS[8] + "|"
-    Day10 = DATA[9][:9] + ", " + DATA[9][10:] + ", " + STATUS[9] + "|"
-    Day11 = DATA[10][:9] + ", " + DATA[10][10:] + ", " + STATUS[10] + "|"
-    Day12 = DATA[11][:9] + ", " + DATA[11][10:] + ", " + STATUS[11] + "|"
-    Day13 = DATA[12][:9] + ", " + DATA[12][10:] + ", " + STATUS[12] + "|"
-    Day14 = DATA[13][:9] + ", " + DATA[13][10:] + ", " + STATUS[13] + "|"
+    days = {i: DATA[i - 1][:9] + ",&nbsp;""&nbsp;High/Low:" + DATA[i - 1][10:] + ",&nbsp;&nbsp;" + STATUS[i - 1] + "&nbsp;&nbsp;" + "<br/><br/>" for i in range(1, 15)}
 
-    period4 = DATA[0][:9] + "-" + DATA[3][:9]
-    period7 = DATA[0][:9] + "-" + DATA[6][:9]
-    period10 = DATA[0][:9] + "-" + DATA[9][:9]
-    period14 = DATA[0][:9] + "-" + DATA[13][:9]
+    Days = {i: DATA[i - 1][:9] + ", " + DATA[i - 1][10:] + ", " + STATUS[i - 1] + "|" for i in range(1, 15)}
+
+    periods = {i: DATA[0][:9] + "-" + DATA[i - 1][:9] for i in range(3, 15)}
+
+    Total4 = days[1] + days[2] + days[3] + days[4]
+    Total7 = days[1] + days[2] + days[3] + days[4] + days[5] + days[6] + days[7]
+    Total10 = days[1] + days[2] + days[3] + days[4] + days[5] + days[6] + days[7] + days[8] + days[9] + days[10]
+    Total14 = days[1] + days[2] + days[3] + days[4] + days[5] + days[6] + days[7] + days[8] + days[9] + days[10] + days[11] + days[12] + days[13] + days[14]
+
+    total4 = Days[1] + Days[2] + Days[3] + Days[4]
+    total7 = Days[1] + Days[2] + Days[3] + Days[4] + Days[5] + Days[6] + Days[7]
+    total10 = Days[1] + Days[2] + Days[3] + Days[4] + Days[5] + Days[6] + Days[7] + Days[8] + Days[9] + Days[10]
+    total14 =  Days[1] + Days[2] + Days[3] + Days[4] + Days[5] + Days[6] + Days[7] + Days[8] + Days[9] + Days[10] + Days[11] + Days[12] + Days[13] + Days[14]
 
     con = sqlite3.connect("capitals.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
 
-    total4 = Day1 + Day2 + Day3 + Day4
-    total7 = Day1 + Day2 + Day3 + Day4 + Day5 + Day6 + Day7
-    total10 = Day1 + Day2 + Day3 + Day4 + Day5 + Day6 + Day7 + Day8 + Day9 + Day10
-    total14 = Day1 + Day2 + Day3 + Day4 + Day5 + Day6 + Day7 + Day8 + Day9 + Day10 + Day11 + Day12 + Day13 + Day14
+    the_option = {
+        '4 days': (periods[4], Total4, total4),
+        'a week': (periods[7], Total7, total7),
+        '10 days': (periods[10], Total10, total10),
+        '2 weeks': (periods[14], Total14, total14)}
 
-    if limit == "4 days":
+    period, Total1, Total2 = the_option[limit]
 
-        cur.execute('''SELECT City, period FROM data WHERE City=? AND period=? ''', (PLACE[0], period4))
-        exists = cur.fetchall()
-        if not exists:
-            output = PLACE[0] + "&nbsp;&nbsp;" + period4 + "<br/><br/>" + day1 + day2 + day3 + day4
-            cur_execute2("INSERT INTO data VALUES(?, ?, ?)", PLACE[0], period4, Day1 + Day2 + Day3 + Day4)
-            print("not exists")
-            return output
-        else:
+    cur.execute('''SELECT City, period FROM data WHERE City=? AND period=? ''', (PLACE[0], period))
+    exists = cur.fetchall()
+    if not exists:
+        output = PLACE[0] + "&nbsp;&nbsp;" + period + "<br/><br/>" + Total1
+        cur_execute2("INSERT INTO data VALUES(?, ?, ?)", PLACE[0], period, Total2)
+        print("not exists")
+        return output
+    else:
 
-            cur.execute('''SELECT City, period, Days FROM data WHERE City=? AND period=? AND Days=?''', (PLACE[0], period4, total4))
-            exists1 = cur.fetchall()
-            for row in exists1:
-                # return tuple(row)
-                PLACE[0], period4, total4 = row
-                return "From your History..." + "<br/><br/>" +PLACE[0], period4 + "<br/><br/>" + total4
+        cur.execute('''SELECT City, period, Days FROM data WHERE City=? AND period=? AND Days=?''', (PLACE[0], period, Total2))
+        exists1 = cur.fetchall()
+        for row in exists1:
+            # return tuple(row)
+            PLACE[0], period, Total2 = row
+            return "From your History..." + "<br/><br/>" +PLACE[0], period + "<br/><br/>" + Total2
 
-
-
-    if limit == "a week":
-        cur.execute('''SELECT City, period FROM data WHERE City=? AND period=?''', (PLACE[0], period7))
-        exists = cur.fetchall()
-        if not exists:
-            output = PLACE[0] + "&nbsp;&nbsp;" + period7 + "<br/><br/>" + day1 + day2 + day3 + day4 + day5 + day6 + day7
-            cur_execute2("INSERT INTO data VALUES(?, ?, ?)", PLACE[0], period7, Day1 + Day2 + Day3 + Day4 + Day5 + Day6 + Day7)
-            print("not exists")
-            return output
-        else:
-            cur.execute('''SELECT City, period, Days FROM data WHERE City=? AND period=? AND Days=?''', (PLACE[0], period7, total7))
-            exists1 = cur.fetchall()
-            for row in exists1:
-                # return tuple(row)
-                PLACE[0], period7, total7 = row
-                return "From your History..." + "<br/><br/>" + PLACE[0], period7 + "<br/><br/>" + total7
-
-    if limit == "10 days":
-        cur.execute('''SELECT City, period FROM data WHERE City=? AND period=?''', (PLACE[0], period10))
-        exists = cur.fetchall()
-        if not exists:
-            output = PLACE[0] + "&nbsp;&nbsp;" + period10 + "<br/><br/>" + day1 + day2 + day3 + day4 + day5 + day6 + day7 + day8 + day9 + day10
-            cur_execute2("INSERT INTO data VALUES(?, ?, ?)", PLACE[0], period10, Day1 + Day2 + Day3 + Day4 + Day5 + Day6 + Day7 + Day8 + Day9 + Day10)
-            print("not exists")
-            return output
-        else:
-            cur.execute('''SELECT City, period, Days FROM data WHERE City=? AND period=? AND Days=?''', (PLACE[0], period10, total10))
-            exists1 = cur.fetchall()
-            for row in exists1:
-                # return tuple(row)
-                PLACE[0], period10, total10 = row
-                return "From your History..." + "<br/><br/>" + PLACE[0], period10 + "<br/><br/>" + total10
-
-    if limit == "2 weeks":
-        cur.execute('''SELECT City, period FROM data WHERE City=? AND period=?''', (PLACE[0], period14))
-        exists = cur.fetchall()
-        if not exists:
-            output = PLACE[0] + "&nbsp;&nbsp;" + period14 + "<br/><br/>" + day1 + day2 + day3 + day4 + day5 + day6 + day7 + day8 + day9 + day10 + day11 + day12 + day13 + day14
-            cur_execute2("INSERT INTO data VALUES(?, ?, ?)", PLACE[0], period14, Day1 + Day2 + Day3 + Day4 + Day5 + Day6 + Day7 + Day8 + Day9 + Day10 + Day11 + Day12 + Day13 + Day14)
-            print("not exists")
-            return output
-        else:
-            cur.execute('''SELECT City, period, Days FROM data WHERE City=? AND period=? AND Days=?''',(PLACE[0], period14, total14))
-            exists1 = cur.fetchall()
-            for row in exists1:
-                # return tuple(row)
-                PLACE[0], period14, total14 = row
-                return "From your History..." + "<br/><br/>" + PLACE[0], period14 + "<br/><br/>" + total14
 
 @app.route('/')
 def home():
